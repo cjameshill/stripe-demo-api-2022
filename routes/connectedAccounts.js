9c6a28e532cfbe293d011ece0383b90a29ff5b8a
Christopher Hill <cjameshill@gmail.com>
2023-03-07 13:35:30 +0000

    Update connectedAccounts.js

--- routes/connectedAccounts.js
+++ routes/connectedAccounts.js
@@ -6,17 +6,29 @@ const routes = Router();
 const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
 const stripe = require("stripe")(STRIPE_SECRET_KEY);
 
-routes.post("/:connectId/onboarding-link", async (req, res) => {
-    const account = req.params.connectId;
+routes.all("/:connectId/onboarding-link", async (req, res) => {
+  const account = req.params.connectId;
 
-    const accountLink = await stripe.accountLinks.create({
-        account,
-        refresh_url: 'https://stripe-demo-sndjoy.netlify.app/',
-        return_url: 'https://stripe-demo-sndjoy.netlify.app/',
-        type: 'account_onboarding',
-    });
+  const accountLink = await stripe.accountLinks.create({
+    account,
+    refresh_url: "https://stripe-demo-sndjoy.netlify.app/",
+    return_url: "https://stripe-demo-sndjoy.netlify.app/",
+    type: "account_onboarding",
+  });
 
-    return res.send({ accountLink });
-})
+  return res.send({ accountLink });
+});
+routes.all("/:connectId/onboarding-link/update", async (req, res) => {
+  const account = req.params.connectId;
+
+  const accountLink = await stripe.accountLinks.create({
+    account,
+    refresh_url: "https://stripe-demo-sndjoy.netlify.app/",
+    return_url: "https://stripe-demo-sndjoy.netlify.app/",
+    type: "account_update",
+  });
+
+  return res.send({ accountLink });
+});
 
 module.exports = routes;
